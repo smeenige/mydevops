cef9ef2f08fdd373b62e8804cb92eae99160eee7
smeenige <soorajreddy0001@gmail.com>
2017-06-01 00:38:29 +0530

    java chnage

--- SampleJavaApp/src/com/shiva/HelloWorld.java
+++ SampleJavaApp/src/com/shiva/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("hellow git welcome");
+		System.out.println("hellow git welcome again");
 	}
 
 }
