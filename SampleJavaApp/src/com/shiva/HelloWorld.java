98033a8e6e9f56621dab88bd5a9ee5035877b8d9
smeenige <soorajreddy0001@gmail.com>
2017-06-04 14:28:24 +0530

    new line added

--- SampleJavaApp/src/com/shiva/HelloWorld.java
+++ SampleJavaApp/src/com/shiva/HelloWorld.java
@@ -7,6 +7,8 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("hellow git welcome again");
+		System.out.println("code checked in github and added to jenkins");
+		
 	}
 
 }
