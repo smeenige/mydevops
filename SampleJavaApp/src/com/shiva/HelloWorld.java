17c24ca99fdd249e6b0f4c194b19ed46eeffc5ce
smeenige <soorajreddy0001@gmail.com>
2017-06-04 15:02:18 +0530

    added jenkins comments

--- SampleJavaApp/src/com/shiva/HelloWorld.java
+++ SampleJavaApp/src/com/shiva/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hellow git welcome again");
 		System.out.println("code checked in github and added to jenkins");
-		
+		System.out.println("build scheduled for every 2 hours in jenkins");
 	}
 
 }
