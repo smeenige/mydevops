a5a26e94d308b23498a262d7e9513c28b0836c4d
smeenige <soorajreddy0001@gmail.com>
2019-02-19 08:00:44 -0500

    new playbooks

---
+++ playbooks/old_playbooks/ticketlog_relay.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp /opt/CSCO-CMS/DataExtractor/lib/ticketLogExtractor.py /opt/install/
+cp /tmp/ticketLogExtractor.py /opt/CSCO-CMS/DataExtractor/lib/
---
+++ playbooks/roles/build_agent/templates/ant.sh
@@ -0,0 +1,2 @@
+export ANT_HOME={{ ant_base_directory }}/{{ ant_directory }}
+export PATH=$PATH:$HOME/bin:$ANT_HOME/bin
